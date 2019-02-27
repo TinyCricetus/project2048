0493a476ee83db6272a2f086cf87ebc81daecc92
LIZEXI <lizexi@cmcm.com>
2019-02-27 20:21:00 +0800

    完成方块出口动画，以及棋盘和出口布局

---
+++ assets/Script/Animal/CircleAnimal.ts
@@ -0,0 +1,21 @@
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+    
+    @property
+    duration: number = 0;
+    
+    //加载
+    public onLoad (): void {
+        this.cicleForever();
+    }
+        
+    //永远转圈
+    public cicleForever (): void {
+        let seq = cc.sequence(cc.rotateBy(this.duration, 180), cc.rotateBy(this.duration, 180));
+        let actionForever = cc.repeatForever(seq);
+        this.node.runAction(actionForever);
+    }   
+}
